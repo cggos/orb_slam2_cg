3f18cd4eb481eb715891d435910f45f333bdfc67
GaoHongchen <1656738573@qq.com>
2019-05-24 18:48:52 +0800

    update scripts/run_stereo_euroc.sh

--- scripts/run_stereo_euroc.sh
+++ scripts/run_stereo_euroc.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 
-cd ..
+param_num=$#
 
-./Examples/Stereo/stereo_euroc Vocabulary/ORBvoc.txt Examples/Stereo/EuRoC.yaml ../../datasets/MH_01_easy/mav0/cam0/data ../../datasets/MH_01_easy/mav0/cam1/data Examples/Stereo/EuRoC_TimeStamps/MH01.txt
+if [ $param_num -ne 1 ]; then
+    echo "usage: ./run_stereo_euroc.sh <dataset_dir>"
+else
+    cd ..
+    dataset_dir="$1"
+    ./Examples/Stereo/stereo_euroc \
+    Vocabulary/ORBvoc.txt \
+    Examples/Stereo/EuRoC.yaml \
+    $dataset_dir/MH_01_easy/mav0/cam0/data \
+    $dataset_dir/MH_01_easy/mav0/cam1/data \
+    Examples/Stereo/EuRoC_TimeStamps/MH01.txt
+fi
