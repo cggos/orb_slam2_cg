0b644ecc69cb605905d69ff2e1b86f7f3cda9c36
cggos <cggos@outlook.com>
2022-03-18 10:35:02 +0800

    remove class DoubleLinkedList

--- orbslam2/include/link_list.h
+++ orbslam2/include/link_list.h
@@ -16,37 +16,6 @@
 
 namespace cg {
 
-typedef int _T;
-
-class DoubleLinkedList {
- private:
-  typedef struct Node {
-    _T data;
-    Node* last;
-    Node* next;
-    Node() : last(nullptr), next(nullptr) {}
-  } Node;
-
- public:
-  DoubleLinkedList();
-  ~DoubleLinkedList();
-
-  bool empty() const;
-  int size() const;
-
-  Node* make_node();
-
-  _T front() const;
-  _T back() const;
-
-  void push_front(const _T& data);
-  void push_back(const _T& data);
-
- private:
-  Node* head_;
-  Node* tail_;
-};
-
 template <typename T>
 class List {
   // ---------------------- Node -----------------------
